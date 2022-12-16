7ce34439c978f23aee8ef1068186f14da1f9134a
Maria Juliana <102608201+majuolyveira@users.noreply.github.com>
2022-12-16 11:02:48 -0300

    Add files via upload

---
+++ n2/Sistema.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sistema;
+
+/**
+ *
+ * @author pc
+ */
+public class Sistema {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
